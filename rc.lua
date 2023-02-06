037f68d598c855b14517c194992cf6f504dc8a98
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-06 22:17:55 +0800

    feat: change shortcut keys for adjusting master client width
    i had the default ones overlapping with my custom shortcuts for
    switching tags
    
    also adjustted the increments from 0.05 to 0.025

--- rc.lua
+++ rc.lua
@@ -315,9 +315,9 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
+    awful.key({ modkey,           }, "]",     function () awful.tag.incmwfact( 0.025)          end,
               {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
+    awful.key({ modkey,           }, "[",     function () awful.tag.incmwfact(-0.025)          end,
               {description = "decrease master width factor", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
               {description = "increase the number of master clients", group = "layout"}),
