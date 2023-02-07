44390eed0fb7b6308ee7264f1efec09da7bf0e47
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-07 21:06:31 +0800

    feat: remove minimize shortcuts
    this in preparation for dealing with task list without buttons

--- rc.lua
+++ rc.lua
@@ -392,17 +392,17 @@ globalkeys = gears.table.join(
         end,
                 { description = "select previous", group = "layout" }),
 
-        awful.key({ modkey, "Control" }, "n",
-                function()
-                    local c = awful.client.restore()
-                    -- Focus restored client
-                    if c then
-                        c:emit_signal(
-                                "request::activate", "key.unminimize", { raise = true }
-                        )
-                    end
-                end,
-                { description = "restore minimized", group = "client" }),
+        --awful.key({ modkey, "Control" }, "n",
+        --        function()
+        --            local c = awful.client.restore()
+        --            -- Focus restored client
+        --            if c then
+        --                c:emit_signal(
+        --                        "request::activate", "key.unminimize", { raise = true }
+        --                )
+        --            end
+        --        end,
+        --        { description = "restore minimized", group = "client" }),
 
 -- Prompt
         awful.key({ modkey }, "r", function()
@@ -452,13 +452,13 @@ clientkeys = gears.table.join(
             c.ontop = not c.ontop
         end,
                 { description = "toggle keep on top", group = "client" }),
-        awful.key({ modkey, }, "n",
-                function(c)
-                    -- The client currently has the input focus, so it cannot be
-                    -- minimized, since minimized clients can't have the focus.
-                    c.minimized = true
-                end,
-                { description = "minimize", group = "client" }),
+        --awful.key({ modkey, }, "n",
+        --        function(c)
+        --            -- The client currently has the input focus, so it cannot be
+        --            -- minimized, since minimized clients can't have the focus.
+        --            c.minimized = true
+        --        end,
+        --        { description = "minimize", group = "client" }),
         awful.key({ modkey, }, "m",
                 function(c)
                     c.maximized = not c.maximized
