184b3deb2e5bb54d011b112e780e8cd904f34b58
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-09 04:57:49 +0800

    feat: add svp to floating windows
    svp should always be floating

--- rc.lua
+++ rc.lua
@@ -658,6 +658,7 @@ awful.rules.rules = {
             "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
             "Wpa_gui",
             "veromix",
+            "SVPManager",
             "xtightvncviewer" },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
