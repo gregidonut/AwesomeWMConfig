2c2daefb0d349edeee4ffa30c2171bf77a69ce0a
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-12 22:05:52 +0800

    feat: add another spacer widget between battery and systray
    i thought this was going to fix that issue where the notification window
    comes up when i hover over it then move my cursor over the systray but
    i don't know

--- rc.lua
+++ rc.lua
@@ -266,6 +266,7 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             -- mykeyboardlayout,
             wibox.widget.systray(),
+            wibox.widget.textbox("    "), --spacer
             --battery arc widget
             batteryarc_widget({
                 show_current_level = true,
